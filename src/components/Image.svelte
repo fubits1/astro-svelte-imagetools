279409bc6665ae9b6b1c264aa674ce9658025fd6
fubits <ilja_s@protonmail.com>
2023-09-19 16:46:52 +0200

    feat: extend image to figure with caption

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,20 +1,22 @@
 <script>
   import {importImageByFilename} from "../utils/vite.js"
   export let filename;
+  export let alt = 'TODO'
+  export let caption = null
+  const image = importImageByFilename(filename);
 
-const image = importImageByFilename(filename);
-
-let isLoaded = false 
-const handleLoad = () => {
-  isLoaded = true
-}
+  let isLoaded = false 
+  const handleLoad = () => {
+    isLoaded = true
+  }
 </script>
 
 <figure use:handleLoad>
   {#if isLoaded}
-  <img src={image} alt="TODO" loading="lazy" />
+  <img src={image} alt={alt} loading="lazy" />
   {/if}
-
-  <!-- <figcaption>TODO</figcaption> -->
+  {#if caption}
+  <figcaption>{caption}</figcaption>
+  {/if}
 </figure>
 
